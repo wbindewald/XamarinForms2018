c85fe0adb8cff8ef4cd635f2ff5475673cb0bc3d
William Cesar Bindewald <wbindewald@hotmail.com>
2019-02-16 18:05:23 -0300

    Aplicativo Finalizado

---
+++ Seção 05/App01_CoonsultarCEP/App01_CoonsultarCEP/App01_CoonsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_CoonsultarCEP.Servico.Modelo;
+using App01_CoonsultarCEP.Servico;
+
+namespace App01_CoonsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //TODO - LOGICA DO PROGRAMA
+
+            //TODO - Validações
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep)) {
+                Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+                RESULTADO.Text = string.Format("Endereço: {0}, {1}, {2}", end.localidade, end.uf, end.logradouro);
+            }
+
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "Cep Inválido! O CEP deve conter 8 caracteres.", "OK");
+                valido = false;
+            }
+
+            int NovoCEP = 0;
+            if (!int.TryParse(cep, out NovoCEP))
+            {
+                DisplayAlert("ERRO", "O Cep deve ser composto apenas por números.", "OK"); 
+                valido = false;
+            }
+
+            return valido;
+        }
+    }
+}
---
+++ Seção 05/App01_CoonsultarCEP/App01_CoonsultarCEP/App01_CoonsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_CoonsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+
+namespace App01_CoonsultarCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return end;            
+        }
+
+    }
+}
